817ff017b4391b26e13ce3b4feebf37a8d0b2aee
hisazakura <luthfanarrayyan@gmail.com>
2023-12-12 10:20:24 +0700

    Add base class

---
+++ Responsi 2/Models/Departemen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Responsi_2.Models
+{
+    public class Departemen
+    {
+        public int Id { get; }
+        public string Nama { get; }
+
+        Departemen(int id, string nama)
+        {
+            Id = id;
+            Nama = nama;
+        }
+    }
+}
---
+++ Responsi 2/Models/Karyawan.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Responsi_2.Models
+{
+    public class Karyawan
+    {
+        public int Id { get; }
+        public string Nama { get; }
+        public string Departemen { get; }
+
+        public Karyawan(int id, string nama, string departemen)
+        {
+            Id = id;
+            Nama = nama;
+            Departemen = departemen;
+        }
+    }
+}
